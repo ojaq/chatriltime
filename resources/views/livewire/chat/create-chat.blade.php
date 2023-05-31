bc3ed3ed3a489ad917c78932124edfadaf232fbd
ojaq <abangojaq@gmail.com>
2023-05-31 14:29:18 +0700

    installed livewire and made component

---
+++ resources/views/livewire/chat/create-chat.blade.php
@@ -0,0 +1,8 @@
+<div>
+    {{-- Care about people's approval and you will be their prisoner. --}}
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Users') }}
+        </h2>
+    </x-slot>
+</div>
