502db4f8cc653d7a1fa89a7b6733aebfc52b685c
ojaq <abangojaq@gmail.com>
2023-06-02 10:38:12 +0700

    send message design

--- resources/views/livewire/chat/send-message.blade.php
+++ resources/views/livewire/chat/send-message.blade.php
@@ -1,6 +1,9 @@
 <div>
     {{-- Nothing in the world is as soft and yielding as water. --}}
     <div class="chatbox_footer">
-        footer
+        <div class="custom_form_group">
+            <input type="text" class="control" placeholder="write message">
+            <button class="submit" type="submit">Send</button>
+        </div>
     </div>
 </div>
