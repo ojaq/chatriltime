e9251eae2f7f77d4646fb0bd76432d1ace0d5e52
ojaq <abangojaq@gmail.com>
2023-06-02 09:40:54 +0700

    design chatbox header

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -14,6 +14,7 @@
         <link rel="stylesheet" href="{{ asset('css/app.css') }}">
         <link rel="stylesheet" href="{{ asset('css/chat.css') }}">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
 
         <!-- Scripts -->
         <script src="{{ asset('js/app.js') }}" defer></script>
--- resources/views/livewire/chat/chatbox.blade.php
+++ resources/views/livewire/chat/chatbox.blade.php
@@ -1,7 +1,26 @@
 <div>
     {{-- The best athlete wants his opponent at his best. --}}
     <div class="chatbox_header">
-        header
+        <div class="return">
+            <i class="bi bi-arrow-left"></i>
+        </div>
+        
+        <div class="img_container">
+            <img src="https://picsum.photos/id/237/200/300" alt="">
+        </div>
+
+        <div class="name">Altop</div>
+        <div class="info">
+            <div class="info_item">
+                <i class="bi bi-telephone-fill"></i>
+            </div>
+            <div class="info_item">
+                <i class="bi bi-image"></i>
+            </div>
+            <div class="info_item">
+                <i class="bi bi-info-circle"></i>
+            </div>
+        </div>
     </div>
 
     <div class="chatbox_body">
